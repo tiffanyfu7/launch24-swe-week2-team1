afa175fc79a6c8d017338c4a17e6ee40c06a136f
Anh Tuong Nguyen <aanhnguyen2023@gmail.com>
2024-05-28 12:34:17 -0700

    created nav bar

---
+++ frontend/SpotifyExtensionApp/src/roots/Discover.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar.jsx';
+
+const Discover = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default Discover
