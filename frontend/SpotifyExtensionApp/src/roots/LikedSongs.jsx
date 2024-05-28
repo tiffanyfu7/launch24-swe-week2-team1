ef4a81d2f8dcfac9f90ad8616e9e34c05f000c0d
Anna Batman <annadbatman@gmail.com>
2024-05-28 15:30:50 -0400

    Shadcn setup

---
+++ frontend/SpotifyExtensionApp/src/roots/LikedSongs.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function LikedSongs() {
+
+    return (
+        <>
+            <div>
+                <Button>Click me</Button>
+            </div>
+        </>
+    );
+}
+
+export default LikedSongs;
