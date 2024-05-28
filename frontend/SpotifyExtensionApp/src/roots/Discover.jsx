64cf13bc20e0f28d4ae872e83116308a297169c2
Abhinav Attaluri <attaluriabhi@gmail.com>
2024-05-28 15:53:06 -0400

    Added base code for backend server

---
+++ frontend/SpotifyExtensionApp/src/roots/Discover.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export const Discover = () => {
+
+
+    return (
+        <> 
+            <h1> Discover Page </h1>
+            <button a href=""> Login with spotify </button>
+        </>
+    )
+}
