508a4f4e0a7faf003344b98e30bbe48eda95c11e
Anh Tuong Nguyen <aanhnguyen2023@gmail.com>
2024-05-29 14:05:41 -0700

    implemented back and forth arrow for forum

--- frontend/SpotifyExtensionApp/src/roots/Forum.jsx
+++ frontend/SpotifyExtensionApp/src/roots/Forum.jsx
@@ -1,35 +1,82 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar.jsx';
 import axios from 'axios';
 import ForumCard from '../components/ForumCard.jsx';
-
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Forum = () => {
-  
   const [forumData, setForumData] = useState(null);
+  const [individualForumId, setIndividualForumId] = useState(null);
+
   const fetchForums = async () => {
-    const response = await axios.get("http://localhost:8000/forum");
-    console.log("forums: ", response.data);
-    setForumData(response.data);
+    try {
+      const response = await axios.get("http://localhost:8000/forum");
+      console.log("forums: ", response.data);
+      setForumData(response.data);
+    } catch (error) {
+      console.error("Error fetching forums:", error);
+    }
   };
 
   useEffect(() => {
     fetchForums();
   }, []);
-  
+
+  const handleForumCardClick = (forumId) => {
+    // Set the individual forum id
+    setIndividualForumId(forumId);
+  };
+
+  const handleBackButtonClick = () => {
+    // Reset the individual forum id to null
+    setIndividualForumId(null);
+  };
+
   return (
     <>
       <NavBar />
       <div className="page-container">
         <h1>Get The Scoop</h1>
-        {forumData &&
-          forumData.map((forum) => 
-            <ForumCard data={forum} />
-          )
-        }
+        
+        {individualForumId ? (
+          <>
+            <button 
+              onClick={handleBackButtonClick}
+              className="back-button" 
+              style={{ 
+                backgroundColor: 'transparent', 
+                border: 'none', 
+                cursor: 'pointer', 
+                padding: '10px' 
+              }}
+            >
+              <FaArrowLeft color="white" size={45} />
+            </button>
+            <h1>You have entered discussion {individualForumId} </h1>
+          </>
+        ) : (
+          forumData &&
+          forumData.map((forum) => (
+            <div key={forum.id}>
+              <ForumCard data={forum} />
+              <button 
+                onClick={() => handleForumCardClick(forum.id)} 
+                className="enter-forum-button" 
+                style={{ 
+                  backgroundColor: 'transparent', 
+                  border: 'none', 
+                  cursor: 'pointer', 
+                  padding: '10px' 
+                }}
+              >
+                <FaArrowRight color="white" size={45} />
+              </button>
+            </div>
+          ))
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default Forum
+export default Forum;
