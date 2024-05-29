e6647122a3ad6a603683e84ab2a845434859e422
Anh Tuong Nguyen <aanhnguyen2023@gmail.com>
2024-05-29 08:27:12 -0700

    fixed positioning of side nav bar

--- frontend/SpotifyExtensionApp/src/components/NavBar.jsx
+++ frontend/SpotifyExtensionApp/src/components/NavBar.jsx
@@ -9,43 +9,42 @@ import {
 import { LuMessagesSquare, LuCompass } from "react-icons/lu";
 import { IoIosPaperPlane } from "react-icons/io";
 import { FaRegHeart } from "react-icons/fa";
+import '../NavBar.css';
 
 const LinkItems = [
-  { name: 'Discover', icon: LuCompass },
-  { name: 'Forum', icon: LuMessagesSquare },
-  { name: 'Inbox', icon: IoIosPaperPlane },
-  { name: 'Library', icon: FaRegHeart },
+  { name: 'Discover', icon: LuCompass, href: '/discover' },
+  { name: 'Forum', icon: LuMessagesSquare, href: '/forum' },
+  { name: 'Inbox', icon: IoIosPaperPlane, href: '/inbox' },
+  { name: 'Library', icon: FaRegHeart, href: '/library' },
 ];
 
 const iconStyle = { color: "black" };
 const iconStyleHover = { color: "white" };
 
 const NavItem = ({ icon, ...rest }) => (
-  <Link href={icon.name} style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }} >
+  <Link href={icon.href} style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
     <Flex
       align="center"
-      p="4"
-      mx="4"
+      p="6"
       borderRadius="lg"
       role="group"
+      className = "nav-item"
       cursor="pointer"
       _hover={{
         bg: '#2B6361',
         color: 'white',
-        borderRadius: '25',
+        borderRadius: '25px',
       }}
       {...rest}>
       {icon.icon && (
         <Icon
-          mr="4"
+          mr="3"
+          ml="3"
+          mt="2"
           fontSize="45"
-          paddingTop={12}
-          paddingBottom={12}
           style={iconStyle}
           _groupHover={iconStyleHover}
           as={icon.icon}
-          paddingRight={10}
-          paddingLeft={10}
         />
       )}
     </Flex>
@@ -58,22 +57,31 @@ const SidebarContent = () => (
     borderRight="1px"
     borderRightColor={useColorModeValue('gray.200', 'gray.700')}
     w={{ base: 'full', md: 60 }}
+    paddingRight="5px"
+    paddingLeft="5px"
+    paddingTop="8px"
     pos="fixed"
+    top="0"
+    left="0"
     h="100vh"
-  
+    className="sidebar"
+    display="flex"
+    flexDirection="column"
   >
-    
-    {LinkItems.map((link) => (
-      <NavItem key={link.name} icon={link} />
-    ))}
+    <Box flex="1">
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link} />
+      ))}
+    </Box>
+    {/*  TODO: ahbinav implement logout functionality */}
+    <Box marginBottom={20} >
+      <button className="logout-button">Log out</button>
+    </Box>
   </Box>
 );
 
 const NavBar = () => {
   return (
-    // <Box minH="100vh" bg='black'>
-    //   <SidebarContent />
-    // </Box>
     <SidebarContent />
   );
 };
