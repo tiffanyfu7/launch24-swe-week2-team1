5f5f7b2119129b89e8d63448c9da3c222ba398f4
Tiffany Fu <tiffany.fu7@gmail.com>
2024-05-29 17:33:13 -0400

    forum card edits

--- frontend/SpotifyExtensionApp/src/components/ForumCard.jsx
+++ frontend/SpotifyExtensionApp/src/components/ForumCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../styles/forum.css'
+import { FaArrowRight } from 'react-icons/fa'
 
 const ForumCard = ({data, handleForumCardClick}) => {
   return (
-    <button className="forum-card-container" onClick={() => handleForumCardClick(forum.id)}>
+    <button className="forum-card-container" onClick={() => handleForumCardClick(data.id)}>
         <img className="album-cover" src={data.albumcover} />
         <h2 className="album-name"> {data.albumname} </h2>
          <div className="right-side"> 
@@ -15,7 +16,7 @@ const ForumCard = ({data, handleForumCardClick}) => {
                 <h5>{data.replies.length}</h5>
                   <h5>Replies</h5>  
             </div>
-            <FaArrowRight color="white" size={45} />
+            <FaArrowRight className="enter-forum-icon" color="white" size={45} />
         </div> 
     </button>
   )
