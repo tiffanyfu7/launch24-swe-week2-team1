1b3a407d506b09e6ef6dfd93966d626d9bca6b30
Pine <pinetcht@gmail.com>
2024-05-29 20:23:55 +0200

    add api calls to spotify in user.js

--- backend/user.js
+++ backend/user.js
@@ -1,8 +1,93 @@
+
+var request = require('request');
 const express = require("express");
 const router = express.Router();
 
 const db = require("./firebase");
 const { collection, getDocs, updateDoc, doc, addDoc, deleteDoc } = require("firebase/firestore");
+
+var client_id = process.env.CLIENT_ID; // your clientId
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+
+async function getToken() {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      body: new URLSearchParams({
+        'grant_type': 'client_credentials',
+      }),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
+      },
+    });
+  
+    return await response.json();
+
+  }
+
+// var authOptions = {
+//     url: 'https://accounts.spotify.com/api/token',
+//     headers: {
+//       'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+//     },
+//     form: {
+//       grant_type: 'client_credentials'
+//     },
+//     json: true
+//   };
+  
+//   request.post(authOptions, function(error, response, body) {
+//     if (!error && response.statusCode === 200) {
+//       var token = body.access_token;
+//       console.log(body);
+//     }
+//   });
+
+
+
+async function getTrackInfo(access_token) {
+    const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + access_token },
+    });
+    console.log(response.json())
+  
+    // return await response.json();
+}
+
+async function getUserInfo(access_token) {
+    const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + access_token },
+    });
+
+  
+    return await response.json();
+}
+
+// const getTrackInfo = async () => {
+//     const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
+//       method: 'GET',
+//       headers: { 'Authorization': 'Bearer ' + accessToken },
+//     });
+
+//     console.log(response.body);
+// }
+
+// getToken().then(response => {
+//     getTrackInfo(response.access_token).then(profile)
+// })
+
+
+  
+  getToken().then(response => {
+    getTrackInfo(response.access_token).then(profile => {
+      console.log(profile)
+    })
+  });
+
+
+
 
 router.get("/", async (req, res) => {
     try {
