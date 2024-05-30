ae8a9f84a503627426599f76666bc9bd76ed472f
Tiffany Fu <tiffany.fu7@gmail.com>
2024-05-30 14:27:16 -0400

    add basic reply box

--- frontend/SpotifyExtensionApp/src/components/Discussion.jsx
+++ frontend/SpotifyExtensionApp/src/components/Discussion.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import Reply from './Reply';
+import ReplyCard from './ReplyCard';
 import { FaRegHeart, FaHeart } from "react-icons/fa";
+import ReplyBox from './ReplyBox';
 
 const Discussion = ({ forumId }) => {
     const [ forumData, setForumData ] = useState(null);
-    const [ like, setLike ] = useState(false);
+    const [like, setLike] = useState(false);
+    const [clickReply, setClickReply] = useState(false);
 
     const fetchForum = async () => {
         const response = await axios.get(`http://localhost:8000/forum/${forumId}`);
@@ -31,7 +33,6 @@ const Discussion = ({ forumId }) => {
 
     return (
         <>
-            {/* <h1>You have entered discussion {forumId} </h1> */}
             {forumData &&
                 <div>
                     <div className="discussion-title-card">
@@ -47,13 +48,23 @@ const Discussion = ({ forumId }) => {
                                 {like ? <FaHeart color="white" size={25} /> : <FaRegHeart color="white" size={25} />}
                                 <p style={{color: "white"}} id="likes-text">{forumData.likes}</p>
                             </button>
-                            <button className="reply-button">Reply</button>
+                            <button onClick={() => setClickReply(true)}
+                                className = "reply-button" > Reply</button>
                         </div>
                     </div>
+                </div>
+            }
+
+            {clickReply &&
+                <ReplyBox set/>
+            }
+            
+            {forumData && 
+                <div>
                     <h1 style={{margin: "20px 0px 8px 10px"}}>Replies</h1>
                     <div className="replies-container">
                         {forumData.replies.map((reply, index) => 
-                            <Reply key={index} reply={reply} />
+                            <ReplyCard key={index} reply={reply} />
                         )}
                     </div>
                 </div>
---
+++ frontend/SpotifyExtensionApp/src/components/ReplyBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ReplyBox = ({setClickedReply}) => {
+    return (
+        <>
+            <button>x</button>
+            <input placeholder="Type a Reply...."></input>
+            <button>Post</button>
+        </>
+    )
+}
+
+export default ReplyBox
