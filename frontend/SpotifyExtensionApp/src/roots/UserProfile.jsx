492cd5be4f4fb4b685e2072fa9ea6d93e1d66f42
Abhinav Attaluri <attaluriabhi@gmail.com>
2024-05-30 13:19:19 -0400

    Updated user profile layout, fixed CSS applied to all pages, and added logo to NavBar

--- frontend/SpotifyExtensionApp/src/roots/UserProfile.jsx
+++ frontend/SpotifyExtensionApp/src/roots/UserProfile.jsx
@@ -6,19 +6,31 @@ const UserProfile = ({ userId }) => {
   
   const { userID, userName } = useContext(AuthContext);
 
+  
+  
   return (
     <>
-      {/* <h1>User Profile</h1> */}
-      <div className="profileContainer"> 
-            <div className="profilePic"></div>
-            <div className="profileBio"> 
-              <h3> {userName} </h3>
-              <h6> Cool One Line Status </h6>
-              <h6> 5 Followers * 30 Following</h6>
-              <button> Edit Profile </button>
-              <button> Inbox </button>
-            </div>
-      </div>  
+      <a href="/Discover"> 
+        <button className="profile-button" style={{width:"60px", height:"30px"}}> 
+          <img src="/backarrow.png" alt="Back"></img>
+        </button> 
+      </a>
+      <div className="main-container"> 
+        <div className="profileContainer"> 
+              <div className="profilePic"></div>
+              <div className="profileBio"> 
+                <h3> {userName} </h3>
+                <h6> 5 Followers * 30 Following</h6>
+                <div className="button-container"> 
+                  <button className="profile-button"> Edit Profile </button>
+                  <button className="profile-button"> Inbox </button>
+                </div>
+              </div>
+        </div>
+        <h4 className="song-header"> Top Songs </h4> 
+
+      
+      </div>
     </>
   )
 }
