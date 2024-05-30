3f7394141b483d490f5158dcc66ba0a15a36aea3
Pine <pinetcht@gmail.com>
2024-05-30 14:30:45 +0200

    add user-top-read scope for authorization

--- backend/user.js
+++ backend/user.js
@@ -1,4 +1,5 @@
-
+// import { useState, useEffect, useContext } from "react";
+// import { AuthContext } from '../components/AuthContext.jsx';
 var request = require("request");
 const express = require("express");
 const router = express.Router();
@@ -12,6 +13,8 @@ const {
   addDoc,
   deleteDoc,
 } = require("firebase/firestore");
+
+// const { userID, userName } = useContext(AuthContext);
 
 var client_id = process.env.CLIENT_ID; // your clientId
 var client_secret = process.env.CLIENT_SECRET; // Your secret
@@ -31,8 +34,8 @@ async function getToken() {
   return await response.json();
 }
   
-async function getTrackInfo(access_token) {
-    const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
+async function getUserInfo(access_token) {
+    const response = await fetch("https://api.spotify.com/v1/me", {
       method: 'GET',
       headers: { 'Authorization': 'Bearer ' + access_token },
     });
@@ -49,8 +52,8 @@ async function getUserTopAllTimeTracks(access_token) {
   return await response.json();
 }
 
-async function getUserTopAllTimeTracks(access_token) {
-    const response = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=long_term", {
+async function getUserTopYearTracks(access_token) {
+    const response = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term", {
       method: "GET",
       headers: { Authorization: "Bearer " + access_token },
     });
@@ -58,35 +61,72 @@ async function getUserTopAllTimeTracks(access_token) {
     return await response.json();
   }
 
+  async function getUserTopMonthlyTracks(access_token) {
+    const response = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", {
+      method: "GET",
+      headers: { Authorization: "Bearer " + access_token },
+    });
+  
+    return await response.json();
+  }
+
+
 // get user info
 getToken().then((response) => {
   getUserInfo(response.access_token).then((profile) => {
-    addDoc(collection(db, "users"), {
-      username: profile.display_name,
-      profilepic: profile.images[1].url,
-      followercount: profile.followers.total,
-      userid: profile.id,
-    });
+    // addDoc(collection(db, "users"), {
+    //   username: profile.display_name,
+    //   profilepic: profile.images[1].url,
+    //   followercount: profile.followers.total,
+    //   userid: profile.id,
+    // });
+    console.log("profile", profile);
   });
 });
 
-// get all time top
+// // get all time top songs
+// getToken().then((response) => {
+//     getUserTopAllTimeTracks(response.access_token).then((topTracks) => {
+//     const usersRef = collection(db, "users");
+//     const q = query(
+//       usersRef,
+//       where("userid", "==", "2i3jv3jp56h1tburma5d38v98")
+//     );
+
+//     const querySnapshot = getDocs(q);
+//     const userToUpdate = querySnapshot[0].id;
+
+//     updateDoc(doc(db, "users", userToUpdate), {
+//         alltimetopsongs: topTracks.items
+//     });
+//   });
+// });
+
+// // get all time top songs
+// getToken().then((response) => {
+//     getUserTopAllTimeTracks(response.access_token).then((topTracks) => {
+//     const usersRef = collection(db, "users");
+//     const q = query(
+//       usersRef,
+//       where("userid", "==", "2i3jv3jp56h1tburma5d38v98")
+//     );
+
+//     const querySnapshot = getDocs(q);
+//     const userToUpdate = querySnapshot[0].id;
+
+//     updateDoc(doc(db, "users", userToUpdate), {
+//         topSongs: topTracks.items
+//     });
+//   });
+// });
+
+// get all time top songs
 getToken().then((response) => {
     getUserTopAllTimeTracks(response.access_token).then((topTracks) => {
-    const usersRef = collection(db, "users");
-    const q = query(
-      usersRef,
-      where("userid", "==", "2i3jv3jp56h1tburma5d38v98")
-    );
-
-    const querySnapshot = getDocs(q);
-    const userToUpdate = querySnapshot[0].id;
-
-    updateDoc(doc(db, "users", userToUpdate), {
-        topSongs: topTracks.items
-    });
+    console.log("topTracks", topTracks);
   });
 });
+
 
 
 
@@ -94,7 +134,6 @@ router.get("/", async (req, res) => {
   try {
     let ret = [];
     const docRef = await getDocs(collection(db, "users"));
-    console.log("docRef in get", docRef);
 
     docRef.forEach((doc) => {
       ret.push({
