3f379082a6c8ae23e7d631ae33a8d245e4029bde
Anh Tuong Nguyen <aanhnguyen2023@gmail.com>
2024-05-30 07:31:00 -0700

    connected the edit profile modal to the edit profile button on user profile page

--- frontend/SpotifyExtensionApp/src/roots/UserProfile.jsx
+++ frontend/SpotifyExtensionApp/src/roots/UserProfile.jsx
@@ -1,26 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../components/AuthContext';
 import '../styles/userProfile.css';
+import EditProfileModal from '../components/EditProfileModal';
 
 const UserProfile = ({ userId }) => {
-  
   const { userID, userName } = useContext(AuthContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <>
-      {/* <h1>User Profile</h1> */}
       <div className="profileContainer"> 
-            <div className="profilePic"></div>
-            <div className="profileBio"> 
-              <h3> {userName} </h3>
-              <h6> Cool One Line Status </h6>
-              <h6> 5 Followers * 30 Following</h6>
-              <button> Edit Profile </button>
-              <button> Inbox </button>
-            </div>
+        <div className="profilePic"></div>
+        <div className="profileBio"> 
+          <h3>{userName}</h3>
+          <h6>Cool One Line Status</h6>
+          <h6>5 Followers * 30 Following</h6>
+          <button onClick={toggleModal}>Edit Profile</button>
+          <button>Inbox</button>
+        </div>
       </div>  
+      {isModalOpen && <EditProfileModal toggleModal={toggleModal} />}
     </>
   )
 }
 
-export default UserProfile
+export default UserProfile;
