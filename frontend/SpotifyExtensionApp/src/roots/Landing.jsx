c28bbe7901ef3b148ec5a0f7084948d5e394dbfc
Anh Tuong Nguyen <aanhnguyen2023@gmail.com>
2024-05-30 07:09:04 -0700

    styling for landing page

--- frontend/SpotifyExtensionApp/src/roots/Landing.jsx
+++ frontend/SpotifyExtensionApp/src/roots/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../components/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Landing.css';
 
 // import NavBar from '../components/NavBar.jsx';
 
@@ -22,8 +23,7 @@ const Landing = () => {
   };
 
   return (
-    <div>
-      {/* <NavBar /> */}
+    <div  className="landingpage-container">
       <h1> Welcome Back </h1>
       <h3> Find your Groove </h3>
       {!accessToken && (
